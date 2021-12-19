a20835701e666e41078a971faee4f5e114ca2fa6
AmnaHAlsayed <89277367+AmnaHAlsayed@users.noreply.github.com>
2021-12-19 12:44:27 +0400

    Update app.js

--- naturalwoodapi/app.js
+++ naturalwoodapi/app.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 //     origin:'http://naturalwooduae.com',
 //     optionsSuccessStatus:200
 // }
-
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
